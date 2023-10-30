68eb7a99f347f787a1a697a403d244204c7a6159
Connor Burns <cpburns17@gmail.com>
2023-10-30 13:36:04 -0400

    added html, css, js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textcontent = "This content added by JavaScript";
+h2.textContent = "This will soon be our wedding website...";
 
 document.querySelector("body").appendChild(h2);
